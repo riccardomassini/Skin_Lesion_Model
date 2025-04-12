d3c97b3afb352430da602f28f36caedbd6534f09
riccardomassini <massiniriccardo585@gmail.com>
2025-04-12 17:07:38 +0200

    change files

--- manage_dataset/fitzpatrick.py
+++ manage_dataset/fitzpatrick.py
@@ -6,57 +6,32 @@ import pandas as pd
 import random
 import os
 
+csv_path = "ALL_DATA/fitzpatrick.csv"
+
 def fitzpatrick_training_data():
     dst_train_data = "TRAIN_DATA/PAN/"
     dst_test_data = "TEST_DATA/PAN/"
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+    headers = {"User-Agent": "myagent"}
 
-    df = pd.read_csv("ALL_DATA/fitzpatrick17k.csv")
-    classes = list(set(df["label"].tolist()))
+    df = pd.read_csv(csv_path)
+    train_url_list = df["train_url"].dropna().tolist()
 
-    class_counts = defaultdict(int)
-    url_dict = defaultdict(list)
-    max_for_class = 500
-    n_for_class = 20
+    url_count = 0
 
-    for _, row in df.iterrows():
-        class_counts[row["label"]] += 1
-        url_dict[row["label"]].append(row["url"])
-
-    for key in url_dict.keys():
-        random.shuffle(url_dict[key])
-
-    url_selected = defaultdict(list)
-
-    for key, value in url_dict.items():
-        url_selected[key] = value[:max_for_class]
-
-    for key, url_list in url_selected.items():
-        url_count = 0
-        for url in url_list:
-            try:
+    for url in train_url_list:
+        try:
+            name = url.split("/")[-1]
+            if name not in os.listdir(dst_train_data) and name not in os.listdir(dst_test_data):
+                image_resp = get(url, headers=headers).content
+                img = Image.open(BytesIO(image_resp))
                 name = url.split("/")[-1]
-                if name not in os.listdir(dst_train_data) and name not in os.listdir(dst_test_data):
-                    image_resp = get(url, headers=headers).content
-                    img = Image.open(BytesIO(image_resp))
-                    name = url.split("/")[-1]
-                    img.save(dst_train_data + name)
-                    url_count += 1
-
-                    print(f"Immagine {name} salvata per la classe {key}, contatore={url_count}!")
-
-                    if url_count >= n_for_class:
-                        break
-
-                else:
-                    print("Già utilizzata...")
-
-            except Exception:
-                print("Errore...")
-                pass
+                img.save(dst_train_data + name)
+                url_count += 1
+                print(f"Immagine {name} salvata, contatore={url_count}!")
+        except Exception as e:
+            print(f"Errore: {e}")
+            pass
 
 
 
@@ -64,50 +39,23 @@ def fitzpatrick_test_data():
     dst_train_data = "TRAIN_DATA/PAN/"
     dst_test_data = "TEST_DATA/PAN/"
 
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
+    headers = {"User-Agent": "myagent"}
 
-    df = pd.read_csv("ALL_DATA/fitzpatrick17k.csv")
-    classes = list(set(df["label"].tolist()))
+    df = pd.read_csv(csv_path)
+    test_url_list = df["test_url"].dropna().tolist()
 
-    class_counts = defaultdict(int)
-    url_dict = defaultdict(list)
-    max_for_class = 100
-    n_for_class = 4
+    url_count = 0
 
-    for _, row in df.iterrows():
-        class_counts[row["label"]] += 1
-        url_dict[row["label"]].append(row["url"])
-
-    for key in url_dict.keys():
-        random.shuffle(url_dict[key])
-
-    url_selected = defaultdict(list)
-
-    for key, value in url_dict.items():
-        url_selected[key] = value[:max_for_class]
-
-    for key, url_list in url_selected.items():
-        url_count = 0
-        for url in url_list:
-            try:
+    for url in test_url_list:
+        try:
+            name = url.split("/")[-1]
+            if name not in os.listdir(dst_train_data) and name not in os.listdir(dst_test_data):
+                image_resp = get(url, headers=headers).content
+                img = Image.open(BytesIO(image_resp))
                 name = url.split("/")[-1]
-                if name not in os.listdir(dst_train_data) and name not in os.listdir(dst_test_data):
-                    image_resp = get(url, headers=headers).content
-                    img = Image.open(BytesIO(image_resp))
-                    name = url.split("/")[-1]
-                    img.save(dst_test_data + name)
-                    url_count += 1
-
-                    print(f"Immagine {name} salvata per la classe {key}, contatore={url_count}!")
-
-                    if url_count >= n_for_class:
-                        break
-
-                else:
-                    print("Già utilizzata...")
-
-            except Exception:
-                print("Errore...")
-                pass
+                img.save(dst_test_data + name)
+                url_count += 1
+                print(f"Immagine {name} salvata, contatore={url_count}!")
+        except Exception as e:
+            print(f"Errore: {e}")
+            pass
